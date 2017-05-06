65c3e96e7d6081e36e36e41c07f4416ed8114081
Brad Stimpson <brad@synaptop.com>
2017-05-06 17:57:52 -0400

    Initial commit with simple structure

---
+++ index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * Adds commas to a number
+ * @param {number} number
+ * @param {string} locale
+ * @return {string}
+ */
+module.exports = function(number, locale) {
+    return number.toLocaleString(locale);
+};/**
+ * Created by brad on 2017-05-06.
+ */
