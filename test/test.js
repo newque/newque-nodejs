f3a0ff36dbbe07726c33fe440a66d21509c96b8b
Brad Stimpson <brad@synaptop.com>
2017-05-08 23:14:01 -0400

    Filling out remaining files and began test.js

--- test/test.js
+++ test/test.js
@@ -1,10 +1,12 @@
 'use strict';
 
 var expect = require('chai').expect;
-var newque = require('../index');
+// var newque = require('../index');
 
-describe('#newque', function () {
-  it('should return count 0', function () {
+/*eslint no-param-reassign:0*/
+describe('#newque', function newque() {
+  /*eslint no-param-reassign:0*/
+  it('should return count 0', function counter() {
     var result = numFormatter(1);
     expect(result).to.equal('1');
   });
